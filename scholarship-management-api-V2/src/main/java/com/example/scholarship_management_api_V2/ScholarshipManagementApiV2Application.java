420839ab0a3c491da2c00b62e274f065db1263e2
Rianadrr <72530@fif.co.id>
2025-09-09 14:57:57 +0700

    Create Project

---
+++ scholarship-management-api-V2/src/main/java/com/example/scholarship_management_api_V2/ScholarshipManagementApiV2Application.java
@@ -0,0 +1,13 @@
+package com.example.scholarship_management_api_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipManagementApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipManagementApiV2Application.class, args);
+	}
+
+}
---
+++ scholarship-management-api-V2/src/test/java/com/example/scholarship_management_api_V2/ScholarshipManagementApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scholarship_management_api_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarshipManagementApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
